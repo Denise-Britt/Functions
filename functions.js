6427a421371883f5509342e187a4ad15f1e156e1
Denise Britt <Denisebritt52@gmail.com>
2020-11-27 09:50:13 -0500

    Function Exercises
    Exercise 1  compares two numbers and returns the largest one of them, logging it out to the console.

---
+++ functions.js
@@ -0,0 +1,23 @@
+// EXERCISE 1
+
+/** function maxOfTwoNumbers takes two numbers as arguments
+ *  and returns the largest of them. */
+
+ function maxOfTwoNUmbers (number1, number2) {
+
+    if (number1 > number2) {
+     
+        console.log(number1);
+    
+    } else if (number2 > number1) {
+        
+        console.log(number2);
+
+    } else {
+        console.log("These numbers are equal");
+    }
+
+ } 
+
+ maxOfTwoNUmbers(13, 10);
+
